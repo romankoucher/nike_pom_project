5547e8629440ea09ed50efa7cfb823dbf6a68e4b
romankoucher <romankoucher@gmail.com>
2025-08-04 22:16:07 +0300

    Remove unnecessary project folders from the repository

--- qa09/condition_exam.py
+++
@@ -1,9 +0,0 @@
-# num1 = 20
-# if (num1 > 25):
-#     print("num1 is more than 25")
-#     num1 = num1 + 5
-#     print(f"the value of num1 is {num1}")
-# else:
-#     print("num1 is lower than 25")
-#     num1 = num1 + 14
-#     print(f"the value of num1 is {num1}")
--- swaglabs_pom/tests/test_product.py
+++
@@ -1,8 +0,0 @@
-
-
-class TestProduct():
-
-    def test_verify_prices_less_than_100(self,setup_swaglabs):
-        page, login_page, product_page = setup_swaglabs
-        login_page.login("standard_user", "secret_sauce")
-        product_page.verify_product_prices()
